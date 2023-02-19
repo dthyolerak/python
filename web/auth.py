37aa9e8bea3ffa59f2bd8d9c212d7604c6ed55c8
McDonnex Thyolera Kamsonga <thyolerak@gmail.com>
2023-02-19 14:40:16 +0200

    allowing post method in login and register

--- web/auth.py
+++ web/auth.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("login")
+@auth.route("login", methods=['GET', 'POST'])
 def login():
+    
     return render_template('login.html')
 
-@auth.route("/register")
+@auth.route("/register", methods=['GET','POST'])
 def register():
     return render_template('register.html')
 
