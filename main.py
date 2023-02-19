62dfcad1b5e433549476d544624605aa960ab28e
McDonnex Thyolera Kamsonga <thyolerak@gmail.com>
2023-02-19 11:25:46 +0200

    running out website and debug has been set to true

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from web import create_app
+from web import create_app #importing create app from web fold
+
+app = create_app() 
+
+if __name__ == '__main__': #run this flask app only if file is main
+    app.run(debug=True) #to run automatic once you change code // turn it off  in production
