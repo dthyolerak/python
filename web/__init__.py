ff82ea01c547a57cee830654491483ec5a660a44
McDonnex Thyolera Kamsonga <thyolerak@gmail.com>
2023-02-19 11:40:00 +0200

    importing registering blueprint in init file

--- web/__init__.py
+++ web/__init__.py
@@ -4,4 +4,9 @@ def create_app(): #define function
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "DEMO-KEY-HERE"
 
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/auth")
     return app
