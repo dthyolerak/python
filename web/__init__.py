a793ebf4bc3bcdbf2b9ba7f79d352975a3e00cce
McDonnex Thyolera Kamsonga <thyolerak@gmail.com>
2023-02-24 07:43:18 +0200

    importing flask_sqlalchemy and in initialize it

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+database = SQLAlchemy()
+DB_NAME =  "database.db"
 def create_app(): #define function
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "DEMO-KEY-HERE"
-
+    app.config['SQLACHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.init_app(app)
     from .views import views
     from .auth import auth
     
